b8b109571c2b76ea120e6a86458f8faaeb66a114
Linmo <30459943+MoorCorPa@users.noreply.github.com>
2023-02-22 15:29:55 +0800

    移动测试

--- Assets/Scripts/Player/BasePlayer.cs
+++ Assets/Scripts/Player/BasePlayer.cs
@@ -1,18 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class BasePlayer : MonoBehaviour
 {
     private const double maxHealth = 100;
     public double basehealth;
-    public int playerInt = 0;
 
-    private PlayerControl control;
+    public PlayerControl control;
 
     virtual public void OnEnable()
     {
         control = KeySetter.input;
+        if (control == null) control = new PlayerControl();
     }
 
     virtual public void Start()
@@ -20,28 +21,56 @@ public class BasePlayer : MonoBehaviour
         basehealth = maxHealth;
     }
 
-    virtual public void OnMove()
+    virtual public void Update()
     {
-        if (basehealth <= 0) return;
+        if (basehealth <= 0) OnDead();
     }
 
-    virtual public void OnJump()
+    virtual public void OnMove(InputAction.CallbackContext context)
     {
-
+        transform.Translate(context.ReadValue<Vector2>());
     }
 
-    virtual public void OnAttack()
-    {
-
-    }
-
-    virtual public void OnORa()
+    virtual public void OnJump(InputAction.CallbackContext context)
     {
 
     }
 
-    virtual public void OnUlt()
+    virtual public void OnAttack(InputAction.CallbackContext context)
     {
 
+    }
+
+    virtual public void OnORa(InputAction.CallbackContext context)
+    {
+
+    }
+
+    virtual public void OnUlt(InputAction.CallbackContext context)
+    {
+
+    }
+
+    virtual public void OnDead()
+    {
+
+    }
+
+    public void BindBlue()
+    {
+        control.PlayerBlue.Move.performed += OnMove;
+        control.PlayerBlue.Jump.performed += OnJump;
+
+        control.PlayerBlue.Move.Enable();
+        control.PlayerBlue.Jump.Enable();
+    }
+
+    public void BindRed()
+    {
+        control.PlayerRed.Move.performed += OnMove;
+        control.PlayerRed.Jump.performed += OnJump;
+
+        control.PlayerRed.Move.Enable();
+        control.PlayerRed.Jump.Enable();
     }
 }
---
+++ Assets/Scripts/Player/PlayerBlue.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBlue : BasePlayer
+{
+    // Start is called before the first frame update
+    override public void Start()
+    {
+        base.Start();
+        BindBlue();
+    }
+
+    // Update is called once per frame
+    override public void Update()
+    {
+        base.Update();
+    }
+}
